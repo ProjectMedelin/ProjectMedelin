50662db6e86b87e4d59f71527f741616f8393633
Wnbj <wnbkrumov@gmail.com>
2017-02-28 20:25:16 +0200

    platform class add

---
+++ src/platform/Platform.java
@@ -0,0 +1,21 @@
+package platform;
+
+import java.util.ArrayList;
+
+import ads.Ads;
+import searchEngine.SearchEngine;
+import users.Developer;
+import users.Employer;
+import users.User;
+
+public class Platform {
+
+	private SearchEngine searchEngine;
+	private ArrayList<Developer> devCatalog;
+	private ArrayList<Employer> empCatalog;
+	private ArrayList<Ads> adsCatalog;
+	private ArrayList<User> users;
+	
+	
+	
+}
